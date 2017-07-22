0bdb5fac6f60428d18b4f7db96f4a0306d565fcf
Pedro P <peucarneiro@hotmail.com>
2017-07-22 13:27:52 -0300

    .

--- main.cpp
+++ main.cpp
@@ -7,7 +7,8 @@ using namespace std;
 
 //g++ main.cpp -IC:\Development\SDL2_MinGW_32Bit\include\SDL2 -IC:\Development\SDL2Image_MinGW_32Bits\include\SDL2 -LC:\Development\SDL2_MinGW_32Bit\lib -LC:\Development\SDL2Image_MinGW_32Bits\lib -lmingw32 -lSDL2main -lSDL2 -lSDL2_Image -o SDL_Game
 
-
+//Defines max size
+const int maxSize = 10;
 
 //Window
 SDL_Window* gWindow = NULL;
@@ -22,10 +23,16 @@ SDL_Surface* foodImage = IMG_Load("Assets/Art/food.png");
 SDL_Surface* wallImage = IMG_Load("Assets/Art/wall.png");
 
 //Player speed (-1 or 0 or 1)  in x and y
-	typedef struct {
-		int x;
-		int y;
-	} speed;
+typedef struct {
+	int x;
+	int y;
+} speed;
+
+//Player position x and y
+typedef struct {
+	int x;
+	int y;
+} position;
 
 void Initialize() {
 
@@ -39,16 +46,23 @@ void Initialize() {
 	gScreenSurface = SDL_GetWindowSurface(gWindow);
 }
 
-void UpdateGameScreen(SDL_Rect pPos[10], SDL_Rect fPos, int size) {
+void UpdateGameScreen(position pPos[maxSize], SDL_Rect fPos, int size) {	
 
 	SDL_FillRect(gScreenSurface, NULL, SDL_MapRGB(gScreenSurface->format, 0, 0, 0));
 
+	//Player rect in screen
+	SDL_Rect targetRect;
+	targetRect.w = 20;
+	targetRect.h = 20;
 
 	for (int i = 0; i < size; i++) {
 		
-		SDL_BlitSurface(pImage, NULL, gScreenSurface, &pPos[i]);
+		targetRect.x = pPos[i].x;
+		targetRect.y = pPos[i].y;
 
+		SDL_BlitSurface(pImage, NULL, gScreenSurface, &targetRect);
 	}
+
 	SDL_BlitSurface(foodImage, NULL, gScreenSurface, &fPos);	
 
 	SDL_UpdateWindowSurface(gWindow);
@@ -70,7 +84,6 @@ int main(int argc, char* agrs[]) {
 
     Initialize();
 
-
     //Snake Size
     int pSize = 1, inFront = 0, nextFront, nextToSave;
 
@@ -79,45 +92,42 @@ int main(int argc, char* agrs[]) {
 	//Check if player wants to quit from game
 	bool quit = false, getInput = true;
 
-	//Player position in window
-	SDL_Rect playerPos[10];
-    playerPos[0].x = 200;
-	playerPos[0].y = 100;
-	playerPos[0].w = 20;
-	playerPos[0].h = 20;
-
-	for (int i = 1; i < 10; i++) {
-
-		playerPos[i].x = 0;
-		playerPos[i].y = 0;
-		playerPos[i].w = 20;
-		playerPos[i].h = 20;
-
-	}
-
 	//Player initial speed		
 	speed pSpeed;
 	pSpeed.x = 1;
 	pSpeed.y = 0;
 
-	speed newPos;
+	//Get the position of where the player will move to
+	position newPos;
+	//Rearange player positions
+	position orderedPos[maxSize];
+	//Player positions
+	position playerPos[maxSize];
+	//First piece of player pos
+	playerPos[0].x = 100;
+	playerPos[0].y = 100;
 
-	speed PosSave[10];
+	//Zero all other pieces position
+	for (int i = 1; i < pSize; i++)	{
 
+		playerPos[i].x = 0;
+		playerPos[i].y = 0;
+
+	}
+
+	//Where food will appear
 	SDL_Rect foodPos;
 	foodPos.x = 80;
 	foodPos.y = 0;
 	foodPos.w = 20;
 	foodPos.h = 20;
 
-
-	//Time Handler
-	int startTime, endTime;
-
-	startTime = SDL_GetTicks();
-
+	//Time Handler  --  SDL_GetTicks() gets the miliseconds passed since Init
+	int startTime = SDL_GetTicks(), endTime;
+	
 	UpdateGameScreen(playerPos, foodPos, pSize);
 
+	
 	while (!quit) {
 		if (SDL_PollEvent(&e)) {
 
@@ -157,6 +167,15 @@ int main(int argc, char* agrs[]) {
 
 		endTime = SDL_GetTicks();
 
+		/*
+
+		Terminar de ajeitar essa parte depois
+		NAO ESQUECA
+		OLHE ISSO
+		AQUI
+		AQUI
+		AQUI
+
 		if (endTime - startTime >= 500) {
 
 			nextFront = inFront - 1;			
@@ -165,8 +184,8 @@ int main(int argc, char* agrs[]) {
 				nextFront = pSize - 1;
 			}
 
-			newPos.x = playerPos[inFront].x + pSpeed.x * 20;
-			newPos.y = playerPos[inFront].y + pSpeed.y * 20;
+			newPos.x = targetPos[inFront].x + pSpeed.x * 20;
+			newPos.y = targetPos[inFront].y + pSpeed.y * 20;
 
 
 			if (newPos.x == foodPos.x && newPos.y == foodPos.y) {
@@ -175,16 +194,16 @@ int main(int argc, char* agrs[]) {
 
 				pSize++;
 
-				PosSave[0].x = newPos.x;
-				PosSave[0].y = newPos.y;
+				orderedPos[0].x = newPos.x;
+				orderedPos[0].y = newPos.y;
 
-				PosSave[pSize - 1].x = playerPos[nextFront].x;
-				PosSave[pSize - 1].y = playerPos[nextFront].y;
+				orderedPos[pSize - 1].x = targetPos[nextFront].x;
+				orderedPos[pSize - 1].y = targetPos[nextFront].y;
 				nextToSave = inFront;
 
 				for (int i = 1; i < pSize - 1; i++) {
-					PosSave[i].x = playerPos[nextToSave].x;
-					PosSave[i].y = playerPos[nextToSave].y;
+					orderedPos[i].x = targetPos[nextToSave].x;
+					orderedPos[i].y = targetPos[nextToSave].y;
 
 					nextToSave++;
 
@@ -195,8 +214,8 @@ int main(int argc, char* agrs[]) {
 
 
 				for (int i = 0; i < pSize - 1; i++) {
-					playerPos[i].x = PosSave[i].x;
-					playerPos[i].y = PosSave[i].y;
+					targetPos[i].x = orderedPos[i].x;
+					targetPos[i].y = orderedPos[i].y;
 
 					nextToSave++;
 
@@ -209,15 +228,16 @@ int main(int argc, char* agrs[]) {
 				nextFront = inFront;				
 
 			} else {
-				playerPos[nextFront].x = newPos.x;
-				playerPos[nextFront].y = newPos.y;
+				targetPos[nextFront].x = newPos.x;
+				targetPos[nextFront].y = newPos.y;
 				inFront = nextFront;
 			}
-
 
 			getInput = true;
 			startTime = SDL_GetTicks();
 		}
+
+		*/
 
 		UpdateGameScreen(playerPos, foodPos, pSize);
 	}
